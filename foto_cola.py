1b9fd682d1082295d0f14f96468acfb016ce7136
Julian Henry <julian@edges.fund>
2022-04-09 13:12:51 -0400

    preparando pra completar o sachete

--- foto_cola.py
+++ foto_cola.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw, ImageOps
 import os
 images = [
     Image.open("/Users/mariopokemon/Desktop/rio/fotos/" + f)
@@ -10,3 +10,12 @@ pdf_path = "/Users/mariopokemon/Desktop/rio/fotos/mar26_mar27.pdf"
 images[0].save(
     pdf_path, "PDF" ,resolution=100.0, save_all=True, append_images=images[1:]
 )
+
+
+def junta_texto():
+    img = Image.open("SAMPLE-IN.png")
+    img = ImageOps.expand(img, border=10, fill=(255,255,255))
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("FONTS/arial.ttf", 36)
+    draw.text((0,0),"Sample Text",(0,255,255),font=font)
+    img.save('sample-out.jpg')
